912ad561b15247a33108c3848504b99612344fef
oukNan <38890764+oukNan@users.noreply.github.com>
2021-06-28 05:27:30 -0700

    update cost data

--- src/data.js
+++ src/data.js
@@ -3,37 +3,37 @@
       FastFood: "American",
       Culture: "Burgers, Fries, salads",
       avgPrice: "22.76 USD",
-      timeElapsed: "5 sec ago",
+      
     },
     {
       FastFood: "Chinese",
       Culture: "Fried rice, stir fry",
-      avgPrice: "45.28 USD",
-      timeElapsed: "10 sec ago",
+      avgPrice: "25.28 USD",
+      
     },
     {
       FastFood: "Convenience",
       Culture: "alcohol, snacks",
-      avgPrice: "341.79 USD",
-      timeElapsed: "3 sec ago",
+      avgPrice: "34.79 USD",
+     
     },
     {
       FastFood: "Mexican",
       Culture: "burritos, tacos, enchiladas, salads",
       avePrice: "9.87 USD",
-      timeElapsed: "4 sec ago",
+      
     },
     {
       FastFood: "Italian",
       Culture: "pizza, spaghetti",
-      avgPrice: "300.99 USD",
-      timeElapsed: "10 sec ago",
+      avgPrice: "$25.00 USD",
+      
     },
     {
       FastFood: "Indian",
       Culture: "noodles, soups",
       avgPrice: "$10.00",
-      timeElapsed: "12 sec ago",
+      
     },
   ];
 
